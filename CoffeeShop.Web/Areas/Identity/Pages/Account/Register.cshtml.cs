84c3fde7486d0c19316881beb01d6f39ad68f6bd
AcademyAspNet <elprofesor.espana@gmail.com>
2025-06-22 14:22:02 +0300

    Added login & register forms errors localization

--- CoffeeShop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CoffeeShop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,8 +74,8 @@ namespace CoffeeShop.Web.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage = "Укажите адрес электронной почты.")]
+            [EmailAddress(ErrorMessage = "Некорректный формат записи адреса электронной почты.")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
@@ -83,8 +83,8 @@ namespace CoffeeShop.Web.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Укажите пароль.")]
+            [StringLength(100, ErrorMessage = "Пароль должен содержать от {2} до {1} символов.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -95,7 +95,7 @@ namespace CoffeeShop.Web.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
             public string ConfirmPassword { get; set; }
         }
 
